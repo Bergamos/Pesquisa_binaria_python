ed46f05159a6b07098b815ec0297ec1c843e7153
bergamos <alisson.bergs@gmail.com>
2022-02-28 15:48:49 -0300

    pesquisa_binaria

---
+++ pesquisa_binaria.py
@@ -0,0 +1,20 @@
+def pesquisar_binaria(lista, item):
+    baixo = 0
+    alto = len(lista) - 1
+
+    while baixo <= alto:
+        meio = int((baixo + alto) / 2)
+        chute = lista[meio]
+
+        if chute == item:
+            return meio
+        if chute > item:
+            alto = meio - 1
+        else:
+            baixo = meio + 1
+    return None
+
+minha_lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(pesquisar_binaria(minha_lista, 2))
+
